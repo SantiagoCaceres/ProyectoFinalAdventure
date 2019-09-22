bc508336a249819c64c187ddae32f69dbf01e51d
Mateo-sudo <55211097+Mateo-sudo@users.noreply.github.com>
2019-09-22 01:04:20 -0500

    Agregamos la clase login
    Falta pulir...
    pr.tx es el archivo que contiene los usuarios y contraseña

---
+++ Comienzo/login.h
@@ -0,0 +1,18 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+class Login
+{
+public:
+    Login();
+    void verificacion();
+    void Registro();
+};
+
+#endif // LOGIN_H
