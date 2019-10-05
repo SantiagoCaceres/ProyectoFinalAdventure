1a6be4dba2138bfeb4c28672140fa51b17dbdf58
Santiago <santiagocaceresv@gmail.com>
2019-10-05 11:36:02 -0500

    "Progreso obstacule"

--- Implementacion en Qt/obstacule.h
+++ Implementacion en Qt/obstacule.h
@@ -2,17 +2,25 @@
 #define OBSTACULE_H
 #include <QGraphicsRectItem>
 #include <QObject>
+#include "personaje.h"
 
 
 class Obstacule:public QObject, public QGraphicsRectItem
 {
     Q_OBJECT
 public:
-    Obstacule();
+    Obstacule(double PX,double PY,double anc, double lar);
     ~Obstacule(){
 
     }
     void colision();
+
+    void setPrincipal(Personaje *value);
+
+private:
+    double PX,PY;
+    Personaje *principal;
+
 };
 
 #endif // OBSTACULE_H
