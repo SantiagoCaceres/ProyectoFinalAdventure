fb160c3dc3de52b91e4c36a966e4dbab6d9dfb7f
Santiago <santiagocaceresv@gmail.com>
2019-10-05 11:51:53 -0500

    Colisiones

--- Implementacion en Qt/obstacule.cpp
+++ Implementacion en Qt/obstacule.cpp
@@ -1,8 +1,12 @@
 #include "obstacule.h"
 
-Obstacule::Obstacule(double PX,double PY,double anc,double lar)
+Obstacule::Obstacule(double _PX,double _PY,double _anc,double _lar)
 {
-setRect(PX,PY,anc,lar);
+setRect(_PX,_PY,_anc,_lar);
+anc=_anc;
+PX=_PX;
+PY=_PY;
+lar=_lar;
 }
 
 void Obstacule::setPrincipal(Personaje *value)
@@ -11,6 +15,17 @@ void Obstacule::setPrincipal(Personaje *value)
 }
 
 void Obstacule::colision(){
+
+    QList<QGraphicsItem *> posterior_items =collidingItems();
+    for (int i = 0, n = posterior_items.size();i<n;i++) {
+        if(typeid(*(posterior_items[i])) == typeid(Personaje)){
+
+            if(principal->getVX()>0 and principal->getPX()<PX+anc){
+
+            }
+
+        }
+      }
 
 
 
